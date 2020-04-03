e5a3efe68e8d82e4fa6604ca00e3a8ea1a8c4eb1
Gabriel Coutinho <gabriel.de.coutinho@accenture.com>
2020-04-03 13:00:56 -0300

    Add function on funcoes and fix string

--- fundamentos/funcoes.js
+++ fundamentos/funcoes.js
@@ -16,3 +16,25 @@ function soma(a, b = 1) { // A partir do ES2015 pode colocar valor default nas v
 console.log(soma(2, 3)) // 5
 console.log(soma(2)) // 3, por não ter valor b assume o 1 default
 console.log(soma()) // NaN, faz a soma de a = undefined e b = 1(default)
+
+// Armazenando uma função em uma variavel
+const imprimeSoma = function (a, b) {
+    console.log(a + b)
+}
+
+imprimeSoma(3, 2)
+
+// Armazenando uma arrow function em uma variavel
+const soma = (a, b) => {
+    return a + b
+}
+
+console.log(soma(3, 2))
+
+// Arrow function retorno implícito
+const subtracao = (a, b) => a - b // com uma linha a arrow func entende que essa linha sera retornada
+console.log(subtracao(2, 3))
+
+// Com apenas um parâmetro nem o () dele eh necessario
+const imprime2 = a => console.log(a)
+imprime2('Bola')
--- fundamentos/string.js
+++ fundamentos/string.js
@@ -8,8 +8,8 @@ console.log(palavra.indexOf('o')) // 1, primeira ocorrencia, para a ultima exist
 console.log(palavra.substring(1)) // do indice X ate o fim
 console.log(palavra.substring(1, 5)) // inclui o primeiro indice, não o último
 
-console.log('Thaynnara '.concat(palavra).concat(' para caralho'))
-console.log('Thaynnara ' + palavra + ' para ccaralho') // fazem a mesma coisa
+console.log('Thaynnara '.concat(palavra).concat(' de mais'))
+console.log('Thaynnara ' + palavra + ' de mais') // fazem a mesma coisa
 
 console.log(palavra.replace('g', 7)) // Case sensitive
 console.log(palavra.replace("a", 4)) // Gostos4
