286b5fbe72770510fb397301e8861e84e67eca5d
Gabriel Coutinho <gabriel.de.coutinho@accenture.com>
2020-04-03 21:37:58 -0300

    Create atribuicao.js

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,17 @@
+const a = 7
+let b = 3
+
+b += a // b = b + a
+console.log(b) // 10
+
+b -= 4 // b = b - a
+console.log(b) // 6
+
+b *= 2 // b = b * 2
+console.log(b) // 12
+
+b /= 6 // b = b / 6
+console.log(b) // 2
+
+b %= 2 // b = b % 2
+console.log(b) // 0
