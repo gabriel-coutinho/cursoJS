f625d55379d10a529ca6836196490247a11c1d05
Gabriel Coutinho <gabriel.de.coutinho@accenture.com>
2020-04-06 19:40:26 -0300

    Create exponenciacao.js

---
+++ fundamentos/exponenciacao.js
@@ -0,0 +1,12 @@
+// Novo no JS o operador exponencial base **(elevado a) valor
+let base = 2
+let exp = base ** 3
+console.log(exp) // 2 elevado a 3 = 8
+
+// Também eh possivel usar com atribuicao
+base **= 3 
+console.log(base) // base = base ** 3 = 8
+
+// Eh possivel com expoentes negativos
+exp = 2 ** -3
+console.log(exp) // 2 elevado a -3 = 0.125
