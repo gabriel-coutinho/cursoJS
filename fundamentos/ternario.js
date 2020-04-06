c665c7985d15203f25cac5215c1b5bce91879509
Gabriel Coutinho <gabriel.de.coutinho@accenture.com>
2020-04-06 20:07:23 -0300

    Create ternario.js

---
+++ fundamentos/ternario.js
@@ -0,0 +1,5 @@
+// Expressão relacional ? Se for true : Se for false
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
+
+console.log(resultado(7.2)) // Aprovado
+console.log(resultado(6.8)) // Reprovado
